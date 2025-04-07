c90fe7d4da8cd2f6bf606bf1448d8dcde605f145
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-04-07 18:07:10 +0500

    Update HomePage.tsx

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -4,9 +4,17 @@ import styles from "./HomePage.module.css";
 import Categories from "../Categories/Categories";
 import InfoBlock from "../InfoBlock/InfoBlock";
 import SectionWrapper from "../SectionWrapper/SectionWrapper";
+import { useAppDispatch, useAppSelector } from "@/store/store";
+import axios from "axios";
+import { getUser } from "@/store/userSlice";
+import { useRef } from "react";
 
 export default function HomePage() {
+  // const dispatch = useAppDispatch();
+  // dispatch(getUser());
   const { data: products } = useGetProductsQuery(null);
+  const sectionRef = useRef(null);
+  console.log(sectionRef.current);
 
   return (
     <div className={styles.page}>
@@ -14,10 +22,16 @@ export default function HomePage() {
         <section className={styles.categories}>
           <div className={styles.row}>
             <Categories />
-            <InfoBlock />
+            <InfoBlock sectionRef={sectionRef.current} />
           </div>
         </section>
-        <SectionWrapper title="Products" data={products} amount={5} btn />
+        <SectionWrapper
+          ref={sectionRef}
+          title="Products"
+          data={products}
+          amount={5}
+          btn
+        />
         <SectionWrapper
           title="Less than 100$"
           data={products}
