4014a7154670b3f6a8891a7e833400d492878b33
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-04-07 18:06:50 +0500

    Update CartPage.tsx

--- src/components/CartPage/CartPage.tsx
+++ src/components/CartPage/CartPage.tsx
@@ -3,26 +3,47 @@ import Button from "@/UI/Button/Button";
 import styles from "./CartPage.module.css";
 import CartItem from "../CartItem/CartItem";
 import { useAppSelector } from "@/store/store";
+import { useState } from "react";
+import LoginModal from "../LoginModal/LoginModal";
 
 export default function CartPage() {
+  const [openModal, setOpenModal] = useState(false);
+
   const { cart } = useAppSelector((state) => state.cart);
   const totalPrice = cart.reduce((acc, v) => acc + v.price * v.count, 0);
+  const { user } = useAppSelector((state) => state.user);
+
   return (
     <section className={styles.cart}>
+      <LoginModal open={openModal} setOpen={setOpenModal} />
       <div className="conteiner">
         <div className={styles.inner}>
-          <h3 className={styles.title}>Your cart</h3>
-          <div className={styles.col}>
-            {cart.map((item) => (
-              <CartItem key={item.id} {...item} cart={cart} />
-            ))}
-          </div>
-          <div className={styles.row}>
-            <div className={styles.total}>
-              TOTAL PRICE: <span>{totalPrice}$</span>
+          {!user && (
+            <div className={styles.err}>
+              Login to your account
+              <Button onClick={() => setOpenModal(true)} fontWeight={500}>
+                Login
+              </Button>
             </div>
-            <Button>Proceed to checkout</Button>
-          </div>
+          )}
+          {cart.length > 0 ? (
+            <>
+              <h3 className={styles.title}>Your cart</h3>
+              <div className={styles.col}>
+                {cart.map((item) => (
+                  <CartItem key={item.id} {...item} cart={cart} />
+                ))}
+              </div>
+              <div className={styles.row}>
+                <div className={styles.total}>
+                  TOTAL PRICE: <span>{totalPrice}$</span>
+                </div>
+                <Button>Proceed to checkout</Button>
+              </div>
+            </>
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
     </section>
