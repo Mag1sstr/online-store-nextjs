f1253e5b9255c9fd2a3c10782956837791e4b93e
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-04-11 02:07:09 +0500

    Update LikesPage.tsx

--- src/components/LikesPage/LikesPage.tsx
+++ src/components/LikesPage/LikesPage.tsx
@@ -1,16 +1,48 @@
 "use client";
 
-import { useAppSelector } from "@/store/store";
+import { useAppDispatch, useAppSelector } from "@/store/store";
 import styles from "./LikesPage.module.css";
+import { redirect } from "next/navigation";
+import Button from "@/UI/Button/Button";
+import { addCart } from "@/store/cartSlice";
+import { ICart, IProducts } from "@/types/interfaces";
+import { setLikes } from "@/store/likeSlice";
+import { toast } from "react-toastify";
+import { MouseEvent } from "react";
 export default function LikesPage() {
+  const dispatch = useAppDispatch();
   const { likes } = useAppSelector((state) => state.like);
-  console.log(likes);
 
+  function handleClick(event: MouseEvent<HTMLButtonElement>, item: ICart) {
+    event.stopPropagation();
+    dispatch(addCart({ ...item, count: 1 }));
+    dispatch(setLikes(likes.filter((v) => v.id !== item.id)));
+    toast.success("Added to cart");
+  }
   return (
     <section className={styles.like}>
       <div className="conteiner">
         <div className={styles.inner}>
-          <h1>Likes</h1>
+          <div className={styles.col}>
+            {likes.map((item) => (
+              <div
+                onClick={() => redirect(`/product/${item.id}`)}
+                key={item.id}
+                className={styles.card}
+              >
+                <img className={styles.img} src={item.images[0]} alt="image" />
+                <div className={styles.text}>
+                  <h3 className={styles.title}>{item.title}</h3>
+                  <p className={styles.desc}>{item.description}</p>
+                </div>
+                <span className={styles.category}>{item.category.name}</span>
+                <span className={styles.price}>{item.price}$</span>
+                <Button onClick={(event) => handleClick(event, item)}>
+                  Add to cart
+                </Button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
